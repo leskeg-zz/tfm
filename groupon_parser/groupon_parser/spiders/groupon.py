c351bd5efaf12972fa84c44bb7eb924507082584
leskeg <gabriel.leske@gmail.com>
2015-05-12 09:31:08 +0200

    added selenium and clicks in banners

--- groupon_parser/groupon_parser/spiders/groupon.py
+++ groupon_parser/groupon_parser/spiders/groupon.py
@@ -1,13 +1,21 @@
 # -*- coding: utf-8 -*-
 import scrapy
-
+from selenium import webdriver
 
 class GrouponSpider(scrapy.Spider):
     name = "groupon"
     allowed_domains = ["groupon.es"]
     start_urls = (
-        'http://www.groupon.es/',
+        'http://www.groupon.es/getaways',
     )
 
+    def __init__(self):
+        self.driver = webdriver.Firefox()
+
     def parse(self, response):
+    	res = self.driver.get(response.url)
+    	self.driver.find_element_by_id("already-registered-link").click()
+    	self.driver.find_element_by_xpath('//*[@id="search_getaways_widget"]/ul/li[1]/div[2]/a').click()
+    	self.driver.find_element_by_xpath('//*[@id="show_more_deals"]').click()
+    	import ipdb; ipdb.set_trace()
         pass
